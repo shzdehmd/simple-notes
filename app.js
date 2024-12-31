d4f45c6917d201acdc1796929310f49f859be224
Ahmad Shahzad <shzdehmd@gmail.com>
2024-12-31 10:25:50 +0500

    Created the logic to register a user.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const njk = require('nunjucks');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const { DatabaseSync } = require('node:sqlite');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '127.0.0.1';
 
 const title = process.env.TITLE || 'simple-notes';
+
+const db = new DatabaseSync('./sqlite.db');
+
+// CREATE THE DEFAULT USERS TABLE
+db.exec(`
+    CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        fname TEXT,
+        lname TEXT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`);
 
 njk.configure('views', {
     express: app,
@@ -28,8 +43,25 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('registered.');
+    const { fname, lname, username, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const insert = db.prepare('INSERT INTO Users (fname, lname, username, password) VALUES (?, ?, ?, ?)');
+
+    try {
+        insert.run(fname, lname, username, hash);
+    } catch (err) {
+        if (err) {
+            return res.status(500).json({
+                code: 500,
+                error: 'internal server error',
+                message: err.message,
+            });
+        }
+    }
+
+    res.redirect('/');
 });
 
 app.get('/login', (req, res) => {
