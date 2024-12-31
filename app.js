d9659f13e23d7e702f5f0f235ba62799be1d1066
Ahmad Shahzad <shzdehmd@gmail.com>
2024-12-31 08:45:27 +0500

    Installed Nunjucks templating engine.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const njk = require('nunjucks');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '127.0.0.1';
+
+njk.configure('views', {
+    express: app,
+    autoescape: true,
+});
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
 
 app.all('*', (req, res) => {
     res.status(404).json({
